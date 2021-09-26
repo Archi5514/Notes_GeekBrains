5c9ad92a27a44106fdc3e44202d4b8be11716a65
Archi <worldofgamesprotanki@gmail.com>
2021-09-26 13:04:59 +0300

    Message

--- app/src/main/java/com/example/notes_di_room_firebase/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/example/notes_di_room_firebase/viewmodel/BaseViewModel.kt
@@ -1,20 +1,34 @@
 package com.example.notes_di_room_firebase.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
 import kotlin.coroutines.CoroutineContext
 
 open class BaseViewModel<T> : ViewModel(), CoroutineScope {
 
     override val coroutineContext: CoroutineContext by lazy {
-        Dispatchers.Default + Job()
+        Dispatchers.Default + SupervisorJob()
     }
 
-    open val viewStateLiveData = MutableLiveData<VS>()
+    private val viewStateChannel = BroadcastChannel<T>(Channel.CONFLATED)
+    private val errorChannel = Channel<Throwable>()
 
-    open fun getViewState(): LiveData<VS> = viewStateLiveData
+    open val viewStateLiveData = MutableLiveData<T>()
+
+    open fun getViewState(): ReceiveChannel<T> = viewStateChannel.openSubscription()
+    fun getErrorChannel(): Channel<Throwable> = errorChannel
+
+    protected fun setError(error: Throwable) = runBlocking {
+        launch {
+
+        }
+    }
 
 }
